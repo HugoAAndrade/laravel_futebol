5f948a018d2179509633f23ae4e7cf071333e375
Hugo Andrade <hugoalvesandrade@gmail.com>
2024-08-14 15:37:44 +0000

    Ajustado template times

--- resources/views/teams.blade.php
+++ resources/views/teams.blade.php
@@ -7,9 +7,10 @@
     @foreach ($teams as $teamName => $players)
     @php
     $hasGoalkeeper = count(array_filter($players, fn($player) => $player->is_goalkeeper));
+    $totalPlayers = count($players); // Conta o total de jogadores no time
     @endphp
     @if ($hasGoalkeeper && count($players) > 1)
-    <div class="bg-slate-600 shadow-md rounded-lg p-4">
+    <div class="bg-slate-600 shadow-md rounded-lg p-4 flex flex-col justify-between gap-2">
         <h2 class="text-2xl font-semibold mb-2 text-white">{{ ucfirst($teamName) }}</h2>
         <ul class="list-disc pl-5">
             @foreach ($players as $player)
@@ -19,6 +20,7 @@
             </li>
             @endforeach
         </ul>
+        <p class="text-white mb-2 font-semibold">Total de jogadores: {{ $totalPlayers }}</p> <!-- Exibe o total de jogadores -->
     </div>
     @endif
     @endforeach
